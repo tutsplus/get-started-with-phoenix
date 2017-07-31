f47d18c775ceae1669e9440afbb91e3d11d9d240
Markus Mühlberger <markus@mmuehlberger.com>
2017-07-31 03:15:22 -0700

    Use the Artists context to fetch data

--- lib/band_manager_web/controllers/band_controller.ex
+++ lib/band_manager_web/controllers/band_controller.ex
@@ -1,47 +1,21 @@
 defmodule BandManagerWeb.BandController do
   use BandManagerWeb, :controller
 
-  @bands [
-    %{name: "Billy Talent", genre: "Punk Rock"},
-    %{name: "Madonna", genre: "Pop"},
-    %{name: "Nightwish", genre: "Metal", albums: [
-        %{name: "Wishmaster", year: 2000, songs: [
-          %{name: "The Kinslayer", rating: 3.0},
-          %{name: "Wanderlust", rating: 1.3},
-          %{name: "Wishmaster", rating: 4.2},
-          %{name: "Crownless", rating: 4.8},
-          %{name: "Dead Boy's Poem", rating: 2.4}
-        ]},
-        %{name: "Once", year: 2004, songs: [
-          %{name: "Dark Chest of Wonders"},
-          %{name: "Wish I Had an Angel"},
-          %{name: "Nemo"}
-        ]},
-        %{name: "Imaginaerium", year: 2011, songs: [
-
-        ]}
-    ]}
-  ]
+  alias BandManager.Artists
 
   def index(conn, %{"query" => search} = params) do
-    bands = @bands
-    |> Enum.filter fn(b) ->
-      b.name =~ search || b.genre =~ search
-    end
+    bands = Artists.find_bands(search)
 
     conn
     |> put_flash(:search, "Found #{Enum.count(bands)} band(s) matching your search.")
-    |> render "index.html", bands: bands
+    |> render("index.html", bands: bands)
   end
 
   def index(conn, _params) do
-    render conn, "index.html", bands: @bands
+    render conn, "index.html", bands: Artists.list_bands()
   end
 
   def show(conn, %{"id" => id}) do
-    band = @bands
-    |> Enum.find (fn(b) -> b.name == id end)
-
-    render conn, "show.html", band: band
+    render conn, "show.html", band: Artists.get_band!(id)
   end
 end
--- lib/band_manager_web/views/band_view.ex
+++ lib/band_manager_web/views/band_view.ex
@@ -1,12 +1,8 @@
 defmodule BandManagerWeb.BandView do
   use BandManagerWeb, :view
 
-  def albums(band) do
-    band[:albums] || []
-  end
-
   def rating(song) do
-    rating = round(song[:rating] || 2.5)
+    rating = round(2.5)
 
     Enum.map (1..5), fn(i) ->
       content_tag :span, (if i <= rating, do: "★", else: "☆")
